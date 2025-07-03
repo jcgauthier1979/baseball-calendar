7c47a21fb55f264966a136e03b65a970699300d0
jcgauthier1979 <165837259+jcgauthier1979@users.noreply.github.com>
2025-07-03 19:13:26 -0400

    Added timeslots and refactored.

---
+++ src/utils/data.ts
@@ -0,0 +1,90 @@
+// Data
+import * as Consts from "./consts";
+import * as Types from "./types";
+import * as Helpers from "./helpers";
+import Papa from "papaparse";
+
+export async function loadEvents(fileName: string, fileUid: string, eventType: string) {
+  const response = await fetch(`${Consts.APP_PATH}/${fileName}?${fileUid}`);
+  if (!response.ok) throw new Error('Failed to fetch events CSV');
+
+  const csvText = await response.text();
+  const parsed =  Papa.parse<Types.CsvRow>(csvText, {
+    header: true
+  });
+
+  if (parsed.errors.length) {
+    throw new Error(parsed.errors.map(e => e.message).join(', '));
+  }
+
+  const parsedEvents: Types.CalendarEvent[] = parsed.data.map((row, index) => {
+    const start = new Date(`${row.Date}T${row["Start Time"]}:00`);
+    const end = new Date(`${row.Date}T${row["End Time"]}:00`);
+    const uid = row["Venue"] + `${row.Date}T${row["Start Time"]}:00` + `${row.Date}T${row["End Time"]}:00`;
+    const title = eventType == Consts.GAME_EVENT_TYPE ?
+      `${row["Home Team Name"]}\n${row["Away Team Name"]}` : Helpers.filterTeamName(row["Team Names"]);
+    const homeTeam = eventType == Consts.GAME_EVENT_TYPE ?
+      Helpers.filterTeamName(row["Home Team Name"]) : Helpers.filterTeamName(row["Team Names"]);
+    const awayTeam = eventType == Consts.GAME_EVENT_TYPE ?
+      Helpers.filterTeamName(row["Away Team Name"]) : "";
+
+    return {
+      type: eventType,
+      id: index,
+      uid: uid,
+      title: title,
+      homeTeam: homeTeam,
+      awayTeam: awayTeam,
+      name: row["Name"],
+      start: start,
+      end: end,
+      allDay: false,
+      venue: row["Venue"],
+      status: row.Status,
+      comments: row.Comments
+    };
+  });
+
+  return parsedEvents;
+}
+
+export async function loadVenueTimeslots(venueName: string, fileName: string, fileUid: string,
+  parsedGameEventsMap: Map<string, Types.CalendarEvent>,
+  parsedPracticesEventsMap: Map<string, Types.CalendarEvent>) {
+
+  const timeslotsResponse = await fetch(Consts.APP_PATH + '/' + fileName + '?' + fileUid);
+  if (!timeslotsResponse.ok) throw new Error('Failed to fetch timeslots JSON');
+
+  const timeslotsJsonText : Types.TimeslotItem[] = JSON.parse(await timeslotsResponse.text());
+  const parsedTimeslotsEvents: Types.CalendarEvent[] = timeslotsJsonText.map((row, index) => {
+    const start = new Date(`${row.startTime}`);
+    const end = new Date(`${row.endTime}`);
+    const uid = venueName + `${Helpers.formatDate(start)}` + `${Helpers.formatDate(end)}`;
+
+    return {
+      type: Consts.TIMESLOT_EVENT_TYPE,
+      id: index,
+      uid: uid,
+      title: "",
+      homeTeam: "",
+      awayTeam: "",
+      name: "",
+      start: start,
+      end: end,
+      allDay: false,
+      venue: venueName + Consts.TIMESLOT_EVENT_TYPE,
+      status: "",
+      comments: ""
+    };
+  });
+
+  const now = new Date();
+  const filteredParsedTimeslotsEvents = parsedTimeslotsEvents.filter(
+    ts => (
+      !parsedGameEventsMap.has(ts.uid)
+      && !parsedPracticesEventsMap.has(ts.uid)
+      && ts.start > now
+    )
+  );
+  return filteredParsedTimeslotsEvents;
+}
---
+++ src/utils/helpers.ts
@@ -0,0 +1,46 @@
+// Helpers
+import * as Consts from "./consts";
+
+export function filterTeamName(name : string) {
+  var separatorPos = name.indexOf(",");
+  var secondName = "";
+
+  if (separatorPos > 0) {
+    var posSecondTeam = name.indexOf(" - Masculin", separatorPos);
+    if (posSecondTeam > 0) {
+      secondName = "<br />" + name.substring(separatorPos + 1, posSecondTeam);
+    }
+  }
+  
+  var pos = name.indexOf(" - Masculin");
+  if (pos > 0) {
+    name = name.substring(0, pos);
+  }
+
+  name = name.replace(",", "")
+  return `${name}${secondName}`;
+}
+
+export function filterVenue(name : string) {
+  // Remove venue specific prefix
+  var pos = name.lastIndexOf("Parc ");
+
+  if (pos != -1) {
+    name = name.substring(pos + 5);
+  }
+
+  // Remove timeslot suffix
+  name = name.replace(Consts.TIMESLOT_EVENT_TYPE, "");
+  return name;
+}
+
+export function formatDate(date: Date): string {
+  const yyyy = date.getFullYear();
+  const MM = String(date.getMonth() + 1).padStart(2, '0');
+  const dd = String(date.getDate()).padStart(2, '0');
+  const hh = String(date.getHours()).padStart(2, '0');
+  const mm = String(date.getMinutes()).padStart(2, '0');
+  const ss = String(date.getSeconds()).padStart(2, '0');
+
+  return `${yyyy}-${MM}-${dd}T${hh}:${mm}:${ss}`;
+}
